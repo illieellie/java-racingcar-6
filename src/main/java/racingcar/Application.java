0ad034933b262693fe9ea396be9ea792869d2034
imellie <ssul4666@naver.com>
2024-05-17 17:08:50 +0900

    application-field-write

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
+    List<String> cars = new ArrayList<String>();
+    List<Integer> scores = new ArrayList<Integer>();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application game = new Application();
+
     }
+
 }
+
+// Randoms.pickNumberInRange(0,9);
+// readLine()
